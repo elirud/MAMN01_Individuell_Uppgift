6dbb42c5148ff1576a77273be937ff6f1ec4d2fa
Elias Rudberg <eli.rud@hotmail.com>
2019-03-27 22:11:34 +0100

    Adds Compass and Accelorometer activities

---
+++ app/src/main/java/com/example/mamn01_individuell_uppgift/AccelorometerActivity.java
@@ -0,0 +1,14 @@
+package com.example.mamn01_individuell_uppgift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccelorometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accelorometer);
+    }
+}
